52cde4bb294da83c2bd2298ac1ec914f3d0e6b3b
Aryan Gandhi <gandhi.aryan1310@gmail.com>
2022-01-16 18:50:04 -0800

    Updates

---
+++ precache-manifest.c5c8d5ea71a2fb8d2b8aaa51b159a6c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e294b44b04818430ef50d3bb8c238368",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f0147474801fbc1268d8",
+    "url": "/Portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/Portfolio/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/Portfolio/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "f0147474801fbc1268d8",
+    "url": "/Portfolio/static/js/main.9052b70f.chunk.js"
+  },
+  {
+    "revision": "dbe8f43fbbbf76d04830",
+    "url": "/Portfolio/static/js/runtime~main.dd438538.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Portfolio/static/media/logo.06e73328.svg"
+  }
+]);
